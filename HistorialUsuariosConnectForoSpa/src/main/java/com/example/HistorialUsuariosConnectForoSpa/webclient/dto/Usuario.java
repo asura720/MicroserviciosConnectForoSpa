6933aae17a5ae61ab779e3ac22874fe9b9434b9a
asura720 <ric.diaza@duocuc.cl>
2025-05-26 02:15:25 -0400

    Cambio nombre variable dto
    Se cambia el nombre de la variable nombre_usuario a nombreUsuario

---
+++ HistorialUsuariosConnectForoSpa/src/main/java/com/example/HistorialUsuariosConnectForoSpa/webclient/dto/Usuario.java
@@ -0,0 +1,10 @@
+package com.example.HistorialUsuariosConnectForoSpa.webclient.dto;
+
+import lombok.Data;
+
+
+@Data
+public class Usuario {
+    private Long id_usuario;
+    private String nombreUsuario;
+}
